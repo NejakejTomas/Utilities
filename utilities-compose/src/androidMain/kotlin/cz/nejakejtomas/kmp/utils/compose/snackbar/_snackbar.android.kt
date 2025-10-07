a259992b1a4f7b6723b37994c6f66ce69169039b
Tomáš Kiec <tomas@kiec.cz>
2025-10-07 17:21:12 +0200

    Add format arguments to android snackbar

--- utilities-compose/src/androidMain/kotlin/cz/nejakejtomas/kmp/utils/compose/snackbar/_snackbar.android.kt
+++ utilities-compose/src/androidMain/kotlin/cz/nejakejtomas/kmp/utils/compose/snackbar/_snackbar.android.kt
@@ -5,6 +5,14 @@ import androidx.annotation.StringRes
 
 fun Snackbar.show(context: Context, @StringRes stringRes: Int) = show(context.getString(stringRes))
 
+fun Snackbar.show(context: Context, @StringRes stringRes: Int, vararg formatArgs: Any) =
+    show(context.getString(stringRes, *formatArgs))
+
 @Suppress("unused")
 context(context: Context)
 fun Snackbar.show(@StringRes stringRes: Int) = show(context, stringRes)
+
+@Suppress("unused")
+context(context: Context)
+fun Snackbar.show(@StringRes stringRes: Int, vararg formatArgs: Any) =
+    show(context, stringRes, *formatArgs)
