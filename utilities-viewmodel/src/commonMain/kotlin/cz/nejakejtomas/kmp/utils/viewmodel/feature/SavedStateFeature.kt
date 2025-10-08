85eef8bfaeb0a1cb4322ae468f7555488bebd97b
Tomáš Kiec <tomas@kiec.cz>
2025-10-08 15:45:00 +0200

    Enforce Savable in SavedStateFeature

--- utilities-viewmodel/src/commonMain/kotlin/cz/nejakejtomas/kmp/utils/viewmodel/feature/SavedStateFeature.kt
+++ utilities-viewmodel/src/commonMain/kotlin/cz/nejakejtomas/kmp/utils/viewmodel/feature/SavedStateFeature.kt
@@ -1,16 +1,17 @@
 package cz.nejakejtomas.kmp.utils.viewmodel.feature
 
 import androidx.lifecycle.SavedStateHandle
+import cz.nejakejtomas.kmp.utils.core.serialization.Savable
 import kotlinx.coroutines.flow.MutableStateFlow
 
-interface SavedStateFeature<SavedState> {
+interface SavedStateFeature<SavedState : Savable> {
     val savedState: MutableStateFlow<SavedState>
 
     companion object {
         private val SAVED_STATE_KEY =
             "${SavedStateFeature::class.qualifiedName ?: throw IllegalStateException()}.SAVED_STATE"
 
-        operator fun <SavedState> invoke(
+        operator fun <SavedState : Savable> invoke(
             savedStateHandle: SavedStateHandle,
             initialValue: SavedState
         ): SavedStateFeature<SavedState> {
